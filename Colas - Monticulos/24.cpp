31dce374d12fbf1d6fc471f30d611c8e7cb97a82
Mauricio Abbati <darldil@users.noreply.github.com>
2018-10-06 11:59:18 +0200

    Ejercicio 24 - Montículos
    Corregido.

--- Colas - Monticulos/24.cpp
+++ Colas - Monticulos/24.cpp
@@ -31,16 +31,16 @@ bool resuelveCaso(){
 }
 
 int main() {// ajustes para que cin extraiga directamente de un fichero
-	#ifndef DOMJUDGE
-        std::ifstream in("casos.txt");
-        auto cinbuf = std::cin.rdbuf(in.rdbuf());
-    #endif
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
     
     while(resuelveCaso());
     // para dejar todo como estaba al principio
-    #ifndef DOMJUDGE
-        std::cin.rdbuf(cinbuf);
-        //system("PAUSE");
-    #endif
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    //system("PAUSE");
+#endif
     return 0;
 }
--- Colas - Monticulos/Mediana.h
+++ Colas - Monticulos/Mediana.h
@@ -12,42 +12,42 @@ public:
     ~ColaMedianas() {}
     
     void push(const int& x) {
-		if (_colamax.empty()) _colamax.push(x);
-		else { // en caso contrario, se introduce en un monticulo u otro
-			if (x > _colamax.top()) _colamax.push(x);
-			else _colamin.push(x);
-			recolocar();
-		}
-
+        if (_colamax.empty()) _colamax.push(x);
+        else { // en caso contrario, se introduce en un monticulo u otro
+            if (x > _colamax.top()) _colamax.push(x);
+            else _colamin.push(x);
+            recolocar();
+        }
+        
     }
     
-    const int& mediana() const {
+    int mediana() const {
         if (!_colamax.empty() && !_colamin.empty() && (_colamax.size() + _colamin.size()) %2 == 0) //Para los pares
             return _colamax.top() + _colamin.top();
         
-		return _colamax.top() * 2; //Impares
+        return _colamax.top() * 2; //Impares
     }
     
     void pop() {
-		_colamax.pop();
+        _colamax.pop();
         recolocar();
     }
     
 private:
     
-	PriorityQueue<int, std::greater<int>> _colamin;
-	PriorityQueue<int, std::less<int>> _colamax;
+    PriorityQueue<int, std::greater<int>> _colamin;
+    PriorityQueue<int, std::less<int>> _colamax;
     
     void recolocar() {
-		if (_colamax.size() == (_colamin.size() + 2)) {
-			_colamin.push(_colamax.top());
-			_colamax.pop();
-		}
-		else if (_colamin.size() == (_colamax.size() + 1)) {
-			_colamax.push(_colamin.top());
-			_colamin.pop();
-		}
-
+        if (_colamax.size() == (_colamin.size() + 2)) {
+            _colamin.push(_colamax.top());
+            _colamax.pop();
+        }
+        else if (_colamin.size() == (_colamax.size() + 1)) {
+            _colamax.push(_colamin.top());
+            _colamin.pop();
+        }
+        
     }
 };
 
